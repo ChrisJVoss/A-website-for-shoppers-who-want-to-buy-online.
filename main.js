a05d76e6ff98042a8347a192b96692a93f6ca37d
Chris Voss <chrisvoss92@gmail.com>
2017-06-14 11:21:01 -0700

    Added event lisnters to the product elements

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var items = [
-  {name: 'Audio-Technica ADTH-AD700' , price: 178.90 , img: "/Users/christophervoss/Desktop/headphone.jpg"},
-  {name: 'Cowin E-7 w/ Acive Noise Cancelling + Bluetooth', price: 65, img: 'https://images-na.ssl-images-amazon.com/images/I/619jaB5tJZL._SL1001_.jpg'},
-  {name: 'Bose QuietComfort 25 Acoustic Noise Cancelling Headphones', price: 299, img: 'https://images-na.ssl-images-amazon.com/images/I/71kgntub7mL._SL1500_.jpg'},
-  {name: 'Sennheiser HD 700', price: 598, img: 'https://images-na.ssl-images-amazon.com/images/I/41XoulU95pL.jpg'}
+  {id: 0, name: 'Audio-Technica ADTH-AD700' , price: 178.90 , img: "/Users/christophervoss/Desktop/headphone.jpg"},
+  {id: 1, name: 'Cowin E-7 w/ Acive Noise Cancelling + Bluetooth', price: 65, img: 'https://images-na.ssl-images-amazon.com/images/I/619jaB5tJZL._SL1001_.jpg'},
+  {id: 2, name: 'Bose QuietComfort 25 Acoustic Noise Cancelling Headphones', price: 299, img: 'https://images-na.ssl-images-amazon.com/images/I/71kgntub7mL._SL1500_.jpg'},
+  {id: 3, name: 'Sennheiser HD 700', price: 598, img: 'https://images-na.ssl-images-amazon.com/images/I/41XoulU95pL.jpg'}
 ]
 
 var $row = document.querySelector('#item-holder')
@@ -24,16 +24,22 @@ function renderItem(product) {
 
   $newItem.classList.add('item-container')
   $newItem.classList.add('col-md-10')
+  $newItem.id = product.id
+
   $itemImg.classList.add('item-img')
   $itemImg.setAttribute('src', product.img)
   $itemImg.classList.add('col-md-4')
+
   $itemName.classList.add('item-name')
   $itemName.classList.add('col-md-4')
   $itemName.textContent = product.name
+
   $itemPrice.classList.add('item-stats')
   $itemPrice.classList.add('col-md-4')
   $itemPrice.textContent = '$' + product.price.toFixed(2)
+
   $lineBreak.classList.add('col-md-12')
+
   $newItem.appendChild($itemImg)
   $newItem.appendChild($itemName)
   $newItem.appendChild($itemPrice)
@@ -42,3 +48,16 @@ function renderItem(product) {
   return $newItem
 }
 displayItems(items)
+
+// Issue-2
+var $itemContainer = document.querySelectorAll('div.item-container')
+for (var i = 0; i < $itemContainer.length; i++) {
+  $itemContainer[i].addEventListener('click', function(event) {
+    var target = event.target
+    console.log(target.id)
+    if (target.id === '') {
+      var parent = target.parentElement
+      console.log(parent)
+    }
+  })
+}
