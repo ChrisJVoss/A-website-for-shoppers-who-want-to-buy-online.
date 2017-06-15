d24669d245bbf3fdb0986d17cf2b6c17073390d3
Chris Voss <chrisvoss92@gmail.com>
2017-06-15 13:43:04 -0700

    Trying to remove even listener.

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 var items = [
-  {id: 0, name: 'Audio-Technica ADTH-AD700' , price: 178.90 , img: "/Users/christophervoss/Desktop/headphone.jpg", description: "53 mm drivers reproduce clear treble and midrange. Bobbin-wound CCAW voice coils for superior power handling. Full open-air type headphones produce no sense of pressure on the ears, resulting in a completely natural sensation. Lightweight aluminum honeycomb casing with excellent acoustic properties. Self-adjusting 3D Wing Support provides a comfortable listening experience; total ear-fit design minimizes pressure"},
+  {id: 0, name: 'Audio-Technica ADTH-AD700' , price: 178.90 , img: "/Users/christophervoss/Desktop/headphone.jpg", description: "53 mm drivers reproduce clear treble and midrange. Bobbin-wound CCAW voice coils for superior power handling. Full open-air type headphones produce no sense of pressure on the ears, resulting in a completely natural sensation. Lightweight aluminum honeycomb casing with excellent acoustic properties. Self-adjusting 3D Wing Support provides a comfortable listening experience; total ear-fit design minimizes pressure."},
   {id: 1, name: 'Cowin E-7 w/ Acive Noise Cancelling + Bluetooth', price: 65, img: 'https://images-na.ssl-images-amazon.com/images/I/619jaB5tJZL._SL1001_.jpg'},
   {id: 2, name: 'Bose QuietComfort 25 Acoustic Noise Cancelling Headphones', price: 299, img: 'https://images-na.ssl-images-amazon.com/images/I/71kgntub7mL._SL1500_.jpg'},
   {id: 3, name: 'Sennheiser HD 700', price: 598, img: 'https://images-na.ssl-images-amazon.com/images/I/41XoulU95pL.jpg'}
 ]
 
 var $row = document.querySelector('#item-holder')
-
-function displayItems(itemList) {
-  for (var i = 0; i < itemList.length; i++) {
-    var currentProduct = itemList[i]
-    var $product = renderItem(currentProduct)
-    $row.appendChild($product)
-  }
-}
 
 function renderItem(product) {
   var $newItem = document.createElement('div')
@@ -51,34 +43,42 @@ function renderItem(product) {
 
   return $newItem
 }
+
+function displayItems(itemList) {
+  for (var i = 0; i < itemList.length; i++) {
+    var currentProduct = itemList[i]
+    var $product = renderItem(currentProduct)
+    $row.appendChild($product)
+  }
+  var $itemContainer = document.querySelectorAll('div.item-container')
+  $row.addEventListener('click', function(event) {
+    var targetData = event.target.dataset
+    //var targetButton = event.target.dataset
+    console.log(typeof targetData)
+    selectedItem(targetData.product, $itemContainer)
+    //returnButton(targetButton)
+  })
+}
+
 displayItems(items)
 
 // Issue-2
 
-document.addEventListener('click', function(event) {
-  var targetData = event.target.dataset.product
-  //var targetButton = event.target.dataset
-  console.log(targetData)
-  selectedItem(targetData)
-  //returnButton(targetButton)
-})
-
-function selectedItem(target) {
-  var $itemContainer = document.querySelectorAll('div.item-container')
-  for (var i = 0; i < $itemContainer.length; i++) {
-    if ($itemContainer[i].dataset.product !== target) {
-      $itemContainer[i].style.visibility = 'hidden'
+function selectedItem(target, container) {
+  for (var i = 0; i < container.length; i++) {
+    if (container[i].dataset.product !== target) {
+      container[i].style.visibility = 'hidden'
     }
   }
-  $itemContainer[target].classList.add('selected-item')
+  container[target].classList.add('selected-item')
   scroll(0,0)
-  itemDescription(items, target)
-  createReturnButton(target)
+  itemDescription(items, target, container)
+  createReturnButton(target, container)
+  $row.removeEventListener('click', event)
 }
 
-function itemDescription(list, target) {
-  var $itemContainer = document.querySelectorAll('div.item-container')
-  var splitDescription = list[target].description.split('.')
+function itemDescription(list, target, container) {
+  var splitDescription = list[target].description.split('. ')
   var $descriptionList = document.createElement('ul')
   $descriptionList.classList.add('col-md-8')
   $descriptionList.classList.add('item-description')
@@ -87,27 +87,28 @@ function itemDescription(list, target) {
     $description.textContent = splitDescription[i]
     $descriptionList.appendChild($description)
   }
-  $itemContainer[target].appendChild($descriptionList)
+  container[target].appendChild($descriptionList)
 }
 
-function createReturnButton(target) {
-  var $itemContainer = document.querySelectorAll('div.item-container')
+function createReturnButton(target, container) {
   var $backButton = document.createElement('button')
   $backButton.setAttribute('type', 'button')
   $backButton.setAttribute('data-button', 'return')
   $backButton.classList.add('btn')
   $backButton.classList.add('btn-elegant')
   $backButton.textContent = 'Return'
-  $itemContainer[target].appendChild($backButton)
+  container[target].appendChild($backButton)
+  $backButton.addEventListener('click', function(event){
+    var targetButton = event.target.dataset
+    returnButton(targetButton.return, container)
+  })
 }
 
-/*function returnButton(target) {
-  if ()
-    for (var i = 0; i < $itemContainer.length; i++) {
-      $itemContainer[i].style.visibility = 'visible'
-      $itemContainer[i].classList.remove('selected-item')
-    }
-    var $descriptionList = document.querySelector('ul')//.item-description')
-    $descriptionList.style.visibility = 'hidden'
+function returnButton(target, container) {
+  for (var i = 0; i < container.length; i++) {
+    container[i].style.visibility = 'visible'
+    container[i].classList.remove('selected-item')
+  }
+  var $descriptionList = document.querySelector('ul')//.item-description')
+  $descriptionList.style.visibility = 'hidden'
 }
-*/
